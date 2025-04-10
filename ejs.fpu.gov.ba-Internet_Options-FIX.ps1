021503b4965d150a50ef7a53d72bdda737a68ecc
SolutionHubBosnia <solutionhubbosnia@gmail.com>
2025-04-10 23:22:54 +0200

    Update ejs.fpu.gov.ba-Internet_Options-FIX.ps1

--- ejs.fpu.gov.ba-Internet_Options-FIX.ps1
+++ ejs.fpu.gov.ba-Internet_Options-FIX.ps1
@@ -1,7 +1,7 @@
 # Definisanje domene
 $trustedSite = "ejs.fpu.gov.ba"
 
-# Dodavanje domene u Trusted Sites zonu (Zona 2)
+# Dodavanje domene u Trusted Sites zonu (Zona 3)
 $zonePath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\$trustedSite"
 if (!(Test-Path $zonePath)) {
     New-Item -Path $zonePath -Force | Out-Null
@@ -9,9 +9,9 @@ if (!(Test-Path $zonePath)) {
 Set-ItemProperty -Path $zonePath -Name "*" -Value 2
 Write-Host "✅ Domena $trustedSite dodana u Trusted Sites." -ForegroundColor Green
 
-# Omogućavanje ActiveX kontrola za Trusted Sites (Zona 2)
+# Omogućavanje ActiveX kontrola za Trusted Sites (Zona 3)
 $activeXKeys = @("1001", "1004", "1200", "1201", "1405")
-$zoneSettingsPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+$zoneSettingsPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
 
 foreach ($key in $activeXKeys) {
     if (!(Get-ItemProperty -Path $zoneSettingsPath -Name $key -ErrorAction SilentlyContinue)) {
